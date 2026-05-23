1cfd74a48a42e16f25cd1d8a75496b67376cb050
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-05-23 10:04:03 +0530

    Update 1_Vendor_Dashboard.py

--- pages/1_Vendor_Dashboard.py
+++ pages/1_Vendor_Dashboard.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from utils.styles import load_css
 
+st.set_page_config(layout="wide")
+
+load_css()
+
+st.title("📊 Vendor Dashboard")
+
+st.success(
+    "Welcome to Vendor Dashboard"
+)

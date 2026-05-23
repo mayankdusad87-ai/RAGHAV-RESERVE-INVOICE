5146095ea9ab421ad51f971b1f7dcc2452e0902e
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-05-23 10:20:56 +0530

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+# ==========================================
+# ADMIN AUTH
+# ==========================================
+
+def admin_login():
+
+    password = st.text_input(
+        "Enter Admin Password",
+        type="password"
+    )
+
+    if password == "admin123":
+
+        return True
+
+    return False
